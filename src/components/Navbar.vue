1015a124d7b797d3fed5b9e3ec6b96bbcad3e7ca
ZehraaMA <zahraa@thecodepeople.io>
2023-05-22 00:44:45 +0300

    create a separate component for navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="flex justify-between items-center px-10 py-5">
+    <a href="/" class="text-4xl text-green-900 font-[cursive]">Cookies</a>
+    <nav>
+      <ul class="flex space-x-4">
+        <li>
+          <a href="#" class="text-green-900 text-lg hover:text-amber-900"
+            >Home</a
+          >
+        </li>
+        <li>
+          <a href="#" class="text-green-900 text-lg hover:text-amber-900"
+            >Cookies</a
+          >
+        </li>
+        <li>
+          <custom-button
+            class="border-4 border-green-900 px-4 rounded-lg hover:bg-green-900 hover:text-white hover:-translate-y-1 hover:scale-110 duration-300"
+            @click="handleButtonClick"
+          >
+            Add Cookie
+          </custom-button>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+import CustomButton from "./CustomButton.vue";
+
+const emit = defineEmits(["openModal"]);
+
+const handleButtonClick = () => {
+  emit("openModal");
+};
+</script>
