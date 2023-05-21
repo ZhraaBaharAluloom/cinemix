77a741893feaa3fde0137bdf2f89a64785310f7d
ZehraaMA <zahraa@thecodepeople.io>
2023-05-22 00:45:16 +0300

    create a reusable custom button

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,17 @@
+<template>
+  <button @click="handleClick">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup>
+const emit = defineEmits(["click"]);
+
+const handleClick = () => {
+  emit("click");
+};
+</script>
+
+<style scoped>
+/* No custom styles needed */
+</style>
