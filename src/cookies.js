308aafec7a5b60cc343352d50753892aaeba3d48
ZehraaMA <zahraa@thecodepeople.io>
2023-05-22 00:43:06 +0300

    create a file that contains dummy data

---
+++ src/cookies.js
@@ -0,0 +1,68 @@
+const cookies = [
+  {
+    name: "Chocolate Chip Cookies",
+    ingredients: [
+      " 1 cup (2 sticks or 226g) unsalted butter, softened",
+      "1 cup (200g) granulated sugar",
+      " 1 cup (220g) packed brown sugar",
+      " 2 large eggs",
+      " 1 teaspoon vanilla extract",
+      " 3 cups (384g) all-purpose flour",
+      " 1 teaspoon baking soda",
+      "1/2 teaspoon salt",
+      " 2 cups (340g) chocolate chips (semi-sweet or milk chocolate, or a combination) ",
+    ],
+    image:
+      "https://images.pexels.com/photos/14000525/pexels-photo-14000525.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    creator: "Zahraa",
+    tags: ["#Sweet", "#Vanilla", "#Chocolate"],
+  },
+  {
+    name: "Soft and Chewy Lemon Cookies",
+    ingredients: [
+      "1/2 cup unsalted butter, softened",
+      "3/4 cup granulated sugar",
+      "2 tablespoons light brown sugar, packed",
+      "1 large egg",
+      "1 tablespoon lemon extract",
+      "2 tablespoons lemon zest",
+      "1 tablespoon honey",
+      "yellow food coloring, optional and as desired",
+      "2 cups flour",
+      "2 teaspoons cornstarch",
+      "pinch salt, optional and to taste",
+      "1 teaspoon baking soda",
+      "2 tablespoons lemon juice",
+      "confectioners’ sugar for dusting or lemon glaze + lemon zest, optional",
+    ],
+    image:
+      "https://www.averiecooks.com/wp-content/uploads/2014/08/lemoncookies-16-735x735.jpg",
+    creator: "Hamza",
+    tags: ["#Sweet", "#Lemon", "#Chocolate"],
+  },
+  {
+    name: "Christmas Cookies and Spices",
+    ingredients: [
+      "1/2 cup unsalted butter, softened",
+      "3/4 cup granulated sugar",
+      "2 tablespoons light brown sugar, packed",
+      "1 large egg",
+      "1 tablespoon lemon extract",
+      "2 tablespoons lemon zest",
+      "1 tablespoon honey",
+      "yellow food coloring, optional and as desired",
+      "2 cups flour",
+      "2 teaspoons cornstarch",
+      "pinch salt, optional and to taste",
+      "1 teaspoon baking soda",
+      "2 tablespoons lemon juice",
+      "confectioners’ sugar for dusting or lemon glaze + lemon zest, optional",
+    ],
+    image:
+      "https://images.pexels.com/photos/10479623/pexels-photo-10479623.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    creator: "Hamza's Sister",
+    tags: ["#Sweet", "#Lemon", "#Chocolate"],
+  },
+];
+
+export default cookies;
