ba0060bb330239fd38b5b823cced432dfde9acc6
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-05-23 15:54:45 +0300

    add custom color to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#07152e",
+        secondary: "#9d0208",
+      },
+    },
   },
   plugins: [],
 };
