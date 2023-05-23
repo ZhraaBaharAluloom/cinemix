0a12a329995b397274f1ada3032f5a26703082f5
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-05-23 16:01:14 +0300

    Welcome Text, Query Search, Popular Movies Fetch
    This commit implements the following changes:
    
    Adds a welcome text to the header of the application with a transition effect.
    Implements a function that takes a query from the user and returns the movie(s) that match the query. The results are then saved in the movie list.
    Implements another function that fetches a list of popular movies. The fetched list is then saved in the movie list.
    Calls the function to fetch popular movies in the onMounted lifecycle hook. This ensures that when the app is loaded, all the movies will be displayed.

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,117 @@
 <template>
-  <div
-    class="bg-cover bg-center min-h-[100vh]"
-    style="
-      background-image: linear-gradient(
-          rgba(255, 255, 255, 0.8),
-          rgba(255, 255, 255, 0)
-        ),
-        url('https://images.pexels.com/photos/5618031/pexels-photo-5618031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
-    "
-  >
-    <Navbar @openModal="openModal" />
-
-    <CookieList
-      :cookiesList="cookiesList"
-      @removeCookie="removeCookie"
-      @selectedCookieToEdit="selectedCookieToEdit"
-      @openModal="openModal"
-    />
-    <Transition>
-      <AddOrEditCookieFrom
-        v-if="open"
-        :oldCookie="oldCookie"
-        :isEditing="isEditing"
-        @addCookie="addCookie"
+  <div class="bg-primary">
+    <header
+      class="w-full bg-cover bg-center min-h-[100vh]"
+      style="
+        background-image: linear-gradient(
+            rgba(255, 255, 255, 0),
+            rgba(4, 21, 46, 1)
+          ),
+          url('https://images.pexels.com/photos/4722571/pexels-photo-4722571.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+      "
+    >
+      <Navbar @openModal="openModal" @getMovie="getMovie" />
+      <div
+        class="text-4xl text-white flex flex-col justify-center items-center h-[80vh] font-serif"
+      >
+        <p>
+          Welcome to
+          <span class="text-secondary px-2 text-6xl font-[cursive]">
+            CineMix</span
+          >,
+        </p>
+        <p>your ultimate destination for all things cinema!</p>
+      </div>
+    </header>
+    <div class="flex justify-center items-start gap-7 flex-wrap px-24 py-14">
+      <MovieList
+        v-for="(movie, index) in moviesList"
+        :key="index"
+        :movie="movie"
+        @removeMovie="removeMovie"
+        @selectedMovieToEdit="selectedMovieToEdit"
         @openModal="openModal"
-        @editCookie="editCookie"
+      />
+    </div>
+    <Transition>
+      <AddOrEditMovieFrom
+        v-if="open"
+        :oldMovie="oldMovie"
+        :isEditing="isEditing"
+        @addMovie="addMovie"
+        @openModal="openModal"
+        @editMovie="editMovie"
       />
     </Transition>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import cookies from "./cookies";
-
-import AddOrEditCookieFrom from "./components/AddOrEditCookieFrom.vue";
-import CookieList from "./components/CookieList.vue";
+import { onMounted, ref } from "vue";
+import movies from "./movies";
+import AddOrEditMovieFrom from "./components/AddOrEditMovieFrom.vue";
+import MovieList from "./components/MovieList.vue";
 import Navbar from "./components/Navbar.vue";
+import axios from "axios";
 
-let cookiesList = ref(cookies);
+let moviesList = ref([]);
 let open = ref(false);
 let isEditing = ref(false);
-let oldCookie = ref({});
+let oldMovie = ref({});
 
-const addCookie = (newCookie) => {
-  cookiesList.value.push(newCookie);
+const addMovie = (newMovie) => {
+  moviesList.value.push(newMovie);
   open.value = false;
 };
 
-const removeCookie = (cookieIndex) => {
+const removeMovie = (movieIndex) => {
   const confirmDelete = confirm(
-    "Are you absolutely positive you want to demolish this delicious cookie? It may shed a tear or two... 🍪😐"
+    "Are you absolutely positive you want to demolish this delicious movie? It may shed a tear or two... 🍪😐"
   );
   if (confirmDelete) {
-    cookiesList.value.splice(cookieIndex, 1);
-    alert("Cookie successfully obliterated! 💥😄");
+    moviesList.value.splice(movieIndex, 1);
+    alert("Movie successfully obliterated! 💥😄");
   } else {
-    alert("Phew! The cookie has been spared. It breathes a sigh of relief. 🍪");
+    alert("Phew! The movie has been spared. It breathes a sigh of relief. 🍪");
   }
 };
 
-const editCookie = (currentCookie, editedCookie) => {
-  const foundCookie = cookiesList.value.indexOf(currentCookie);
-  cookiesList.value.map((cookie, index) => {
-    if (foundCookie == index) {
-      Object.assign(cookie, editedCookie);
+const getMovies = async () => {
+  try {
+    const response = await axios.get(
+      "https://api.themoviedb.org/3/movie/popular?api_key=9c1bd422dc4c265552caa0ce7241a530"
+    );
+    moviesList.value = response.data.results;
+  } catch (error) {
+    console.log("🚀 ~ file: tmdbServices.js:9 ~ getMovies ~ error:", error);
+  }
+};
+const getMovie = async (movieName) => {
+  try {
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/search/collection?query=${movieName}&page=1&api_key=9c1bd422dc4c265552caa0ce7241a530`
+    );
+
+    moviesList.value = response.data.results;
+  } catch (error) {
+    console.log("🚀 ~ file: tmdbServices.js:97 ~ getMovie ~ error:", error);
+  }
+};
+
+const editMovie = (currentMovie, editedMovie) => {
+  const foundMovie = moviesList.value.indexOf(currentMovie);
+  moviesList.value.map((movie, index) => {
+    if (foundMovie == index) {
+      Object.assign(movie, editedMovie);
     }
     isEditing.value = !isEditing.value;
     open.value = false;
   });
 };
 
-const selectedCookieToEdit = (currentCookie) => {
+const selectedMovieToEdit = (currentMovie) => {
   isEditing.value = true;
-  oldCookie.value = currentCookie;
+  oldMovie.value = currentMovie;
 };
 
 const openModal = () => {
@@ -83,4 +121,8 @@ const openModal = () => {
 
   open.value = !open.value;
 };
+
+onMounted(() => {
+  getMovies();
+});
 </script>
