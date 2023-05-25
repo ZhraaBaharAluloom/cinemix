d63a158bd75ded5ee0b931d2a42c8d163ec2c945
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2023-05-25 22:39:17 +0300

    Clean up the code by removing unnecessary comments

--- src/components/MovieDetail.vue
+++ src/components/MovieDetail.vue
@@ -26,37 +26,12 @@
         <p class="">{{ movieDetail.overview }}</p>
       </div>
     </div>
-    <!-- <div class="flex items-center">
-      <span class="text-lg font-semibold"> Rating: </span>
-      <span class="ml-2">{{ movieDetail.rating }}/10</span>
-    </div> -->
-    <!-- <div class="mt-3">
-      <span class="text-lg font-semibold">Casts:</span>
-           <ul class="list-disc ml-6">
-             <li v-for="cast in movieDetail.casts" :key="cast.id">
-               {{ cast.name }}
-             </li>
-           </ul>
-         </div> -->
-    <!-- <div class="mt-10">
-           <h2 class="text-2xl font-semibold mb-3">Related Movies</h2>
-           <div class="grid grid-cols-2 md:grid-cols-4 gap-4">
-             <div
-               class="flex flex-col items-center cursor-pointer"
-               v-for="relatedMovie in movie.relatedMovies"
-               :key="relatedMovie.id"
-             >
-               <img
-                 :src="relatedMovie.poster"
-                 alt="Movie Poster"
-                 class="w-full h-auto mb-2 rounded-lg"
-               />
-               <span>{{ relatedMovie.title }}</span>
-             </div>
-           </div>
-         </div> -->
     <router-link to="/">
-      <p>Back</p>
+      <p
+        class="border-4 border-secondary w-24 px-2 py-1 text-center rounded-md transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-100 hover:bg-gray-100 hover:text-primary duration-400 hover:border-white text-lg"
+      >
+        Back
+      </p>
     </router-link>
   </div>
 </template>
@@ -65,8 +40,6 @@
 import { useRoute } from "vue-router";
 import instance, { API_KEY } from "../http";
 import { computed, onMounted, ref } from "vue";
-// import MovieList from "./MovieList.vue";
-// import mdilThumbUpIcon from "vue-material-design-icons/mdilThumbUp.vue";
 
 const route = useRoute();
 const movieDetail = ref({});
@@ -78,14 +51,11 @@ const getMovieDetails = async () => {
     );
 
     movieDetail.value = response.data;
-    console.log(
-      "🚀 ~ file: MovieDetail.vue:78 ~ getMovieDetails ~ movieDetail.value:",
-      movieDetail.value
-    );
   } catch (error) {
     console.log("🚀 ~ file: tmdbServices.js:97 ~ getMovie ~ error:", error);
   }
 };
+
 const imgURL = computed(() => {
   return `https://image.tmdb.org/t/p/w500/`;
 });
@@ -94,7 +64,3 @@ onMounted(() => {
   getMovieDetails();
 });
 </script>
-
-<style>
-/* Add Tailwind CSS classes or custom styles here */
-</style>
